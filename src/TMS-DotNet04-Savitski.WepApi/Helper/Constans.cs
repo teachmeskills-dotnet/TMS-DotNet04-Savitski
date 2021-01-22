6b3d41618183c1ab2e546b9a6a2857106c8717d0
Dmitry Savitski <dima.savickiy2001@gmail.com>
2021-01-22 16:11:46 +0300

    feat: add helpers classes to implement logic of tg bot

---
+++ src/TMS-DotNet04-Savitski.WepApi/Helper/Constans.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMS_DotNet04_Savitski.WepApi.Helper
+{
+    public class Constans
+    {
+        public const string Url = "https://www.nbrb.by/api";
+    }
+}
---
+++ src/TMS-DotNet04-Savitski.WepApi/Models/Rates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMS_DotNet04_Savitski.WepApi.Models
+{
+    public class Rates
+    {
+        public int Cur_ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Cur_Abbreviation { get; set; }
+        public int Cur_Scale { get; set; }
+        public string Cur_Name { get; set; }
+        public double Cur_OfficialRate { get; set; }
+    }
+}
