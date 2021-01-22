986ceb5633ff28133fc404de1e996ef9ba2553fd
Dmitry Savitski <dima.savickiy2001@gmail.com>
2021-01-22 16:06:52 +0300

    feat: add new interfaces

---
+++ src/TMS-DotNet04-Savitski.WepApi/Interfaces/ITelegramCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace TMS_DotNet04_Savitski.WepApi.Interfaces
+{
+    public interface ITelegramCommand
+    {
+        string Name { get; }
+
+        Task Execute(Message message ,ITelegramCommand client);
+
+        bool Contains(Message message);
+    }
+}
